a8c74bf9d58074e2e18ac9fce97900e749476021
User1 <weaverjon36@yahoo.com>
2018-02-12 11:05:34 -0500

    Two text fields have just been added

--- SwiftFun/ViewController.swift
+++ SwiftFun/ViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+ //   var buttonCount = 0
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    @IBAction func myButton(_ sender: Any) {
+        
+        print(topTextField.text!)
+        print(bottomTextField.text!)
+        
+ /*
+        buttonCount += 1
+        myLabel.text = "Woah Bro, you im-pressed me!"
+        view.backgroundColor = UIColor.red
+        print("Button Press Count: \(buttonCount)")
+        
+        if buttonCount >= 10 {
+            
+            myLabel.text = "Oh my! You've pressed me 10 times!"
+        }
+            
+            if buttonCount >= 15 {
+                myLabel.text = "HOLY COW, 15 TIMES! Here's some green, on me."
+                view.backgroundColor = UIColor.green
+        }
+ */
+    }
+ 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
